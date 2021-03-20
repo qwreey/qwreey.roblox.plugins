7f7bde1c016161f83c7278786dadea4e89bd801b
qwreey75 <qwreey75@gmail.com>
2021-03-20 12:43:24 +0900

    lang file fix

--- IconPack.Plugin/res/Language/ko-kr.lua
+++ IconPack.Plugin/res/Language/ko-kr.lua
@@ -4,4 +4,24 @@ return {
 	["settings"] = "설정";
 	["Settings"] = "설정";
 	["Back"] = "뒤로";
+	["Selected"] = "선택됨 : %s";
+	["ImageID"] = "이미지 ID : %s";
+	["Style"] = "스타일 : %s";
+	["Group"] = "그룹 : %s";
+	["Set"] = "교체";
+	["Insert"] = "추가";
+	["SearchHere"] = "검색하기 ...";
+	["NoFavoritesList"] = "즐겨찾기 항목이 없습니다";
+	["Favorites"] = "즐겨찾기";
+	["AddToFavorites"] = "즐겨찾기에 추가하기";
+	["RemoveFromFavorites"] = "즐겨찾기 해제하기";
+	["Menu_ToolTip"] = "그룹들 보기";
+	["Close"] = "닫기";
+	["Search"] = "검색";
+	["TopBarText"] = "Material 아이콘";
+	["Groups"] = "그룹들";
+	["ResizingFrame"] = "크기 조정중 . . .";
+	["ImageAdded"] = "이미지가 추가되었습니다";
+	["ImageSetted"] = "이미지가 교체되었습니다";
+	["Undo"] = "되돌리기";
 }
