6acef11f81ffed7e3a5e681f8d3fe00889f0d824
qwreey75 <qwreey75@gmail.com>
2021-04-04 18:51:46 +0900

    fontSize setting added (testf)

---
+++ FontViewer.Plugin/res/settings.lua
@@ -0,0 +1,31 @@
+local settings = {};
+
+function settings.init(Info)
+    local Data = Info.Data;
+
+    local settingsTable = {};
+    settings.settingsTable = settingsTable;
+
+    -- data 는 self 호출이 있어야 넘어감
+    settingsTable.FontSize = Data:Load("settingsfontSize"); -- display font size
+    local BindToFontSizeChanged = {};
+    settingsTable.BindToFontSizeChanged = function(func)
+        local funcId = tostring(func);
+        BindToFontSizeChanged[funcId] = func;
+        return function()
+            BindToFontSizeChanged[funcId] = nil;
+        end,funcId;
+    end;
+    settingsTable.setFontSize = function(FontSize)
+        -- 저장시키기
+        Data:Save("settingsfontSize",FontSize);
+        for _,func in pairs(BindToFontSizeChanged) do
+            func(FontSize);
+        end
+    end
+    
+
+    return settingsTable;
+end
+
+return settings;
